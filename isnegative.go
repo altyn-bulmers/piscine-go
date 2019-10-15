59e2b661a19b0af343182bae6a533087c93f95d0
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 05:17:51 +0600

    0204

--- isnegative.go
+++ isnegative.go
@@ -1,4 +1,4 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
@@ -10,12 +10,4 @@ func IsNegative(nb int) {
 	}
 	z01.PrintRune('\n')
 	return
-}
-
-func main() {
-
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
-
 }
