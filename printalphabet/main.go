208fa181ce521b7b0aa80c15b62d418ed6c31d0d
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 01:18:16 +0600

    0201

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,11 +4,6 @@ import (
 	"fmt"
 )
 
-const (
-	Alphabet = "abcdefghijklmnopqrstuvwxyz"
-)
-
-
 func main() {
 	
 	for i := 97; i <= 122; i++ {
@@ -17,10 +12,3 @@ func main() {
 	fmt.Print("\n");
 }
 
-/*func loweralpha() string {
-	p := make([]byte, 26)
-	for i := range p {
-		p[i] = 'a' + byte(i)
-	}
-	return string(p)
-}*/
