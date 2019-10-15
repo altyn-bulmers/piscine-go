81567973a22b46d9d8f1d2396b763679922b5544
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 01:33:43 +0600

    0201

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	
 	for i := 97; i <= 122; i++ {
-		fmt.Print(string(i))
+		z01.PrintRune(i)
 	}
-	fmt.Print("\n");
+	z01.PrintRune('\n')
 }
 
