f98bf18f2c6889dc203bc9476b9941aa04a81bbd
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 17:57:05 +0600

    0205

--- printcomb.go
+++ printcomb.go
@@ -2,22 +2,22 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb() {	
+func PrintComb() {
 	next := false
 	for i := '0'; i <= '9'; i++ {
-        for j := i + 1; j <= '9'; j++ {
-            for k := j + 1; k <= '9'; k++ {
+		for j := i + 1; j <= '9'; j++ {
+			for k := j + 1; k <= '9'; k++ {
 				if next {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 
-				}				
+				}
 				next = true
 				z01.PrintRune(i)
 				z01.PrintRune(j)
 				z01.PrintRune(k)
-			}					
-		}			
-    }
-	z01.PrintRune('\n')	    
+			}
+		}
+	}
+	z01.PrintRune('\n')
 }
