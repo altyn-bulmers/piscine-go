3052d77925d547d0b59a9043d63b1d37dab7f27e
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 18:33:57 +0600

    0206

---
+++ printcomb2.go
@@ -0,0 +1,26 @@
+package pisine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := i; k <= '9'; k++ {
+				for f := j + 1; f <= '9'; f++ {
+
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(f)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+
+		z01.PrintRune('\n')
+	}
+
+}
