167f22e8b7e4f7d6c6120ddc1e59bc4dab2b1e79
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 17:47:10 +0600

    0205

--- printcomb.go
+++ printcomb.go
@@ -9,8 +9,8 @@ func PrintComb() {
         for j := i + 1; j <= '9'; j++ {
             for k := j + 1; k <= '9'; k++ {
 				if next {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
 				
 				next = true
