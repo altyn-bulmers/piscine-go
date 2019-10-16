5ec604b35b1324a8354c229c62869d7f9525fdf9
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-16 21:00:09 +0600

    dfdfs

--- printnbr.go
+++ printnbr.go
@@ -6,21 +6,20 @@ import (
 
 func PrintNbr(n int) {
 
-	t:=1
-		if n<0{
-			t=-1
-			z01.PrintRune('-')
+	t := 1
+	if n < 0 {
+		t = -1
+		z01.PrintRune('-')
+	}
+	if n != 0 {
+		f := (n / 10) * t
+		if f != 0 {
+			PrintNbr(f)
 		}
-		if n!=0{
-			f:=(n/10)*t
-			if f!=0{
-				PrintNbr(f)
-			}
-			k:=((n%10*t))+'0'
-			z01.PrintRune(rune(k))
-			}else{
-				z01.PrintRune('0')
-			}
-		
-}
+		k := (n % 10 * t) + '0'
+		z01.PrintRune(rune(k))
+	} else {
+		z01.PrintRune('0')
+	}
 
+}
