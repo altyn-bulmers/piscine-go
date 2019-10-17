26dd806af28410028cf4485d80d7f2351f81d42a
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-17 19:21:54 +0600

    prints reverse of the str

---
+++ strrev.go
@@ -0,0 +1,20 @@
+package piscine
+
+func StrLen(str string) int {
+	Astr := []rune(str)
+	count := 0
+	for i := range Astr {
+		i++
+		count++
+	}
+	return count
+
+}
+func StrRev(s string) string {
+	Bstr := []rune(str)
+	c := StrLen(str)
+	for i := c - 1; i >= 0; i-- {
+		d := Bstr[i]
+	}
+	return string(d)
+}
