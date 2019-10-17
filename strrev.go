d29b9715414b1353951b51817aed2edbe87a66da
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-17 20:17:27 +0600

    prints reverse str

--- strrev.go
+++ strrev.go
@@ -1,18 +1,12 @@
 package piscine
 
-func StrLen(str string) int {
-	Astr := []rune(str)
-	count := 0
-	for i := range Astr {
-		i++
-		count++
-	}
-	return count
-
-}
 func StrRev(s string) string {
-	LenRune := StrLen(s)
 	Bstr := []rune(s)
+	LenRune := 0
+	for i := range Bstr {
+		i++
+		LenRune++
+	}
 	var reverse string
 	for i := LenRune - 1; i >= 0; i-- {
 		reverse += string(Bstr[i])
