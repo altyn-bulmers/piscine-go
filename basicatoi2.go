38c653b062fb7b6644d758fdc2db5d93d2e48186
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-18 01:00:36 +0600

    atoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,16 +1,5 @@
 package piscine
 
-func Pow(a, b int) int {
-	p := 1
-	for b > 0 {
-		if b&1 != 0 {
-			p *= a
-		}
-		b >>= 1
-		a *= a
-	}
-	return p
-}
 func BasicAtoi2(s string) int {
 	Bstr := []rune(s)
 	Cstr := []rune(s)
