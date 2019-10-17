4a78d1a00f06092e114be2556ceeccde58c9326f
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-17 17:23:53 +0600

    count number of characters in str

--- strlen.go
+++ strlen.go
@@ -1,12 +1,11 @@
 package piscine
 
 import (
-	"github.com/01-edu/z01"
+	"fmt"
 )
 
 func PrintStr(str string) {
 	Astr := []rune(str)
-	for i, v := range Astr {
-		z01.PrintRune(i)
-	}
+	fmt.Println(len(Astr))
+
 }
