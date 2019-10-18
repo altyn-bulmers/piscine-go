504ecb6b64ca70e772e2a8e856335cf730e5ce8a
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-18 19:57:06 +0600

    first attempt

---
+++ raid1a.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main(){
+
+	z01.PrintRune('o')
+
+}
+
+func Raid1a(x, y int) {	
+	k = []rune(x)
+	e = []rune(y)
+
+	lengthk := 0
+	lengthe := 0 
+	for i :=  range k{
+		lengthk = i
+	}
+	for j := range e{
+		lengthe = i
+	}
+
+
+	z01.PrintRune('o')
+
+
+	for i:= range k - 2 {
+		
+	}
+
+}
