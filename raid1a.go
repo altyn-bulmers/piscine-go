f98a0b5d1fcb3de1e18c077e7b41896b6cd7a47a
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-19 17:35:32 +0600

    group assignment my version

--- raid1a.go
+++ raid1a.go
@@ -1,36 +1,73 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func main(){
 
-	z01.PrintRune('o')
+	Raid1a(1,1)
 
 }
 
-func Raid1a(x, y int) {	
-	k = []rune(x)
-	e = []rune(y)
-
-	lengthk := 0
-	lengthe := 0 
-	for i :=  range k{
-		lengthk = i
-	}
-	for j := range e{
-		lengthe = i
-	}
-
-
+func Raid1a(x, y int){
+	
 	z01.PrintRune('o')
+	
+
+	if x == 1 && y == 1{
+		z01.PrintRune('\n')
+		return 
+	} else if x == 1 {
+		for t:=1; t<y-1; t++{ //stolko raz skolko y - 2
+			if t <= 0{
+				break
+			} else {
+				z01.PrintRune('\n')
+				z01.PrintRune('|')
+
+			}
+			if y == 1 {
+				z01.PrintRune('\n')
+				z01.PrintRune('o')
+				return
+			}
+		}
+		z01.PrintRune('\n')
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
+		
+
+	} else {
+		for i:=1; i< x - 1; i++ {
+			z01.PrintRune('-') // 3 dash iz 5
+		}
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
 
 
-	for i:= range k - 2 {
-		z01.PrintRune('-') // 3 dash iz 5
+		for t:=1; t<y-1; t++{ //stolko raz skolko y - 2
+			z01.PrintRune('|')
+			
+			for j:=1; j<x -1;j++ {
+				z01.PrintRune(' ') // 3 pustoty iz 5
+			}
+			z01.PrintRune('|')
+			z01.PrintRune('\n')
+
+		}
+		
+		
+		z01.PrintRune('o')
+			
+		for i:=1; i< x - 1; i++ {
+			z01.PrintRune('-') // 3 dash iz 5
+		}	
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
+		
+
 	}
-
-	for j:= range k -2 {
-		z01.PrintRune(' ') // 3 pustoty iz 5
-	}
+	
 
 }
