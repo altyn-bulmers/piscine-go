d828a85b695bc7dcb2f3dae79f4a3c280717c96c
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-20 15:39:22 +0600

    QUEST-04

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(RecursiveFactorial(8))
+	fmt.Println(RecursiveFactorial(2))
 }
 
 func RecursiveFactorial(nb int) int {
-	if nb >=3 {
-		return RecursiveFactorial(nb -1) + RecursiveFactorial(nb -2)
+	if nb > 1 {
+		return RecursiveFactorial(nb -1) * nb
 	} else {
 		return 1
 	}
