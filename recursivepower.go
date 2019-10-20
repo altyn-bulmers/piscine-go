84130612bebb3f51873a90a17c64a5d3479241ee
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 05:24:13 +0600

    recursion

--- recursivepower.go
+++ recursivepower.go
@@ -1,14 +1,12 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	if power < 1 {
-		return 1
+	if power < 0 {
+		return 0
 	} else {
 
-		return RecursivePower(nb, power-1) * nb 
-
+		return RecursivePower(nb, power-1) * nb
 
 	}
 
-	
 }
