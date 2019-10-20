24f5b6a42d18a1a1eafa9b042cdd67b281154f97
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 05:16:33 +0600

    added statements

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,7 +1,7 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb > 1 {
+	if nb > 1 && nb < 2147483647 {
 		return RecursiveFactorial(nb-1) * nb
 	} else if nb == 0 || nb == 1 {
 		return 1
