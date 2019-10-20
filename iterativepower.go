87c0706de41e511ca28a03f8091c744001230585
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 05:22:04 +0600

    added limits

--- iterativepower.go
+++ iterativepower.go
@@ -1,11 +1,13 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	result := 1
-	
-	for i := 0; i < power; i++ {
-		result *= nb  
+	if power < 0 {
+		return 0
+	} else {
+		result := 1
+		for i := 0; i < power; i++ {
+			result *= nb
+		}
+		return result
 	}
-		
-	return result
 }
