14e42d1c8260053c807ea4c2688d439a4359e3dd
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 09:24:08 +0600

    loop

--- findnextprime.go
+++ findnextprime.go
@@ -1,26 +1,12 @@
 package piscine
 
-// FindNextPrime function returns the first
-// prime number that is equal or superior
-// to the int passed as parameter.
-// The function is optimized to avoid time-outs.
 func FindNextPrime(nb int) int {
-	var nextprime int
-
 	if IsPrime(nb) == true {
-		nextprime = nb
+		return nb
 	} else {
-		for i := nb + 1; i < nb*2; i++ {
-			if IsPrime(i) == true {
-				nextprime = i
-				return nextprime
-			}
+		if IsPrime(nb) == true {
+			return nb
 		}
 	}
-	return nextprime
+	return FindNextPrime(nb + 1)
 }
-
-// IsPrime function returns true if the int
-// passed as parameter is a prime number.
-// Otherwise it returns false.
-// The function is optimized to avoid time-outs.
