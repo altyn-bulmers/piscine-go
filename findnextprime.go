22b72c0155f5f15e5b9cb375ca7906b967403f38
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 09:38:28 +0600

    loop

--- findnextprime.go
+++ findnextprime.go
@@ -3,10 +3,6 @@ package piscine
 func FindNextPrime(nb int) int {
 	if IsPrime(nb) == true {
 		return nb
-	} else {
-		if IsPrime(nb) == true || nb > 2147483647 || nb < -2147483648 {
-			return nb
-		}
 	}
 	return FindNextPrime(nb + 1)
 }
