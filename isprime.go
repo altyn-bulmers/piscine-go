cd902bae2b29bd1944d5aa73093d258d597bec08
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 07:13:02 +0600

    isprime

---
+++ isprime.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	result := true
+	if nb <= 1 {
+		result = false
+	} else {
+		for i := 2; i < nb; i++ {
+			if nb%i == 0 {
+				result = false
+			}
+
+		}
+	}
+	return result
+}
