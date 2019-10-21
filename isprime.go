f25f185f519e1cf3973a3d511f34a5ccda53e040
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 12:04:49 +0600

    SQ

--- isprime.go
+++ isprime.go
@@ -3,15 +3,16 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
-	} 
+	}
 	if nb == 2 {
 		return true
-	} else {
-		for i := 3; i <= Sqrt(nb); i += 2 {
-			if nb%i == 0 {
-			 	return false
-			}
-
+	}
+	if nb%2 == 0 {
+		return false
+	}
+	for i := 3; i <= Sqrt(nb); i += 2 {
+		if nb%i == 0 {
+			return false
 		}
 	}
 	return true
