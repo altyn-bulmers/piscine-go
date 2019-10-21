f22cdbd70c3fc872ec978d676f6fa3b5adfdff57
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 08:44:43 +0600

    QUEST-04

--- findnextprime.go
+++ findnextprime.go
@@ -11,3 +11,18 @@ func FindNextPrime(nb int) int {
 		return n
 	}
 }
+
+func IsPrime(nb int) bool {
+	result := true
+	if nb <= 1{
+		result = false
+	} else {
+		for i := 2; i < nb; i++ {
+			if nb%i == 0 {
+				result = false
+			} 
+			
+		} 
+	}	
+	return result 
+}
