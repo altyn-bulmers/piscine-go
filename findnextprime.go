e9362e7586af76711d05fb475b36873edc31e705
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 09:10:58 +0600

    loop

--- findnextprime.go
+++ findnextprime.go
@@ -4,9 +4,13 @@ func FindNextPrime(nb int) int {
 	if IsPrime(nb) == true {
 		return nb
 	} else {
-		n := nb + 1
-		for IsPrime(n) == true {
-			break
+		n := 0
+		for i := nb + 1; i < nb*2; i++ {
+			n = i
+			if IsPrime(i) == true {
+				return n
+				break
+			}
 		}
 		return n
 	}
