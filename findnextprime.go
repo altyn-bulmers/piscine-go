717653156351f9d07a7ab53917ef7a928adbc929
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-21 09:29:00 +0600

    loop

--- findnextprime.go
+++ findnextprime.go
@@ -4,7 +4,7 @@ func FindNextPrime(nb int) int {
 	if IsPrime(nb) == true {
 		return nb
 	} else {
-		if IsPrime(nb) == true {
+		if IsPrime(nb) == true || nb > 2147483647 || nb < -2147483648 {
 			return nb
 		}
 	}
