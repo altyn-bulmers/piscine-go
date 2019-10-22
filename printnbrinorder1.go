8c939c499a5c3e92c9e8d884ab82906e13a1f5f2
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-22 23:37:25 +0600

    easy version

---
+++ printnbrinorder1.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	var cnt [10]int
+	for n != 0 {
+		cnt[n%10]++
+		n /= 10
+	}
+	for i := 0; i < 10; i++ {
+		for cnt[i] > 0 {
+			z01.PrintRune(rune(i) + '0')
+			cnt[i]--
+		}
+	}
+}
