9796b648a4837f6b26bbc176c86ba91bceb8d168
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-22 16:27:11 +0600

    quest-05

--- alphacount.go
+++ alphacount.go
@@ -4,10 +4,9 @@ func AlphaCount(str string) int {
 	runes := []rune(str)
 	count := 0
 	for i, letter := range runes {
-		if letter >= 'a' && letter <= 'z'|| letter >= 'A' && letter <= 'Z' {
+		if letter >= 'a' && letter <= 'z' || letter >= 'A' && letter <= 'Z' {
 			count++
 		}
 	}
 	return count
 }
-
