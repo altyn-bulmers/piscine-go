b9dbb3ffb49148b96324231c6ab11a93715524fd
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-22 16:26:34 +0600

    quest-05

---
+++ alphacount.go
@@ -0,0 +1,13 @@
+package piscine
+
+func AlphaCount(str string) int {
+	runes := []rune(str)
+	count := 0
+	for i, letter := range runes {
+		if letter >= 'a' && letter <= 'z'|| letter >= 'A' && letter <= 'Z' {
+			count++
+		}
+	}
+	return count
+}
+
