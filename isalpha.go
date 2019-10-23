48d6b51de20cfd562456a9a0ef8e782bf22538e0
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 09:59:41 +0600

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -1,12 +1,5 @@
 package piscine
 
-func check(a rune) bool {
-	if (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9') {
-		return true
-	}
-	return false
-}
-
 func IsAlpha(str string) bool {
 	runes := []rune(str)
 
