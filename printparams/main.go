9ae5334dfa89ab048535f2ee1fef762590819e4a
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 20:52:33 +0600

    06

--- printparams/main.go
+++ printparams/main.go
@@ -10,12 +10,14 @@ func main() {
 	arguments := os.Args
 
 	for i := range arguments {
-		for j := range arguments[i] {
-			runes := []rune(arguments[i])
-			if i != 0 {
-				z01.PrintRune(runes[j])
+		if i != 0 {
+			for j := range arguments[i] {
+				runes := []rune(arguments[i])
+				 {
+					z01.PrintRune(runes[j])
+				}
 			}
+			z01.PrintRune('\n')
 		}
-		z01.PrintRune('\n')
 	}
 }
