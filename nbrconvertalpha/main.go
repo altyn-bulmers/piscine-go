c4b688f855011bd10eb401ad9ae3517fc107901d
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-24 05:51:39 +0600

    n

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -6,33 +6,28 @@ import (
 )
 
 func main() {
-
 	arguments := os.Args[1:]
 	caps := false
-
 	for _, v := range arguments {
 		if v == "--upper" {
 			caps = true
 		}
 	}
-
 	for _, arg := range arguments {
-		numv := 0 
+		numv := 0
 		for _, v := range arg {
-			numv = numv * 10 + int(v - '0')
+			numv = numv*10 + int(v-'0')
 		}
-
 		if numv >= 1 && numv <= 26 {
 			if caps == false {
-				z01.PrintRune (rune(numv + 96))
+				z01.PrintRune(rune(numv + 96))
 			} else {
-				z01.PrintRune (rune(numv + 64))
+				z01.PrintRune(rune(numv + 64))
 			}
 		} else {
 			z01.PrintRune(' ')
 		}
-
 	}
-	z01.PrintRune('\n')			
+	z01.PrintRune('\n')
 
 }
