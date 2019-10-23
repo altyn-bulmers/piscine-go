94e9c06b185e86a8b6deff079e2e70944b174fbd
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 07:55:12 +0600

    cap

--- capitalize.go
+++ capitalize.go
@@ -4,7 +4,7 @@ func Capitalize(s string) string {
 
 	runes := []rune(s)
 	i := 1
-	for i < len(runes)+1 {
+	for i < StrLen(runes)+1 {
 		if (runes[i-1] == ' ' || runes[i-1] == '+' || i == 0) && (runes[i] >= 'a' && runes[i] <= 'z') {
 			runes[i] = runes[i] - 32
 		}
