47bc95f78c79ee9d53446019fd9050ff1d1e01b8
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 20:15:09 +0600

    06

---
+++ printprogramname/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main(){
+	
+	arguments := os.Args
+	name := arguments[0]
+
+	runes := []rune(name)
+	for i := range runes {
+		z01.PrintRune(runes[i])
+	}
+	z01.PrintRune('\n')
+}
