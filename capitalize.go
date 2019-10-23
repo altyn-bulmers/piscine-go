07e4012ff3a656b13926c976daf8b092d58890c1
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 08:00:11 +0600

    cap

--- capitalize.go
+++ capitalize.go
@@ -1,10 +1,15 @@
 package piscine
 
 func Capitalize(s string) string {
-
 	runes := []rune(s)
+	length := 0
+	for j := range runes {
+		j++
+		length++
+	}
+
 	i := 1
-	for i < StrLen(runes)+1 {
+	for i < length+1 {
 		if (runes[i-1] == ' ' || runes[i-1] == '+' || i == 0) && (runes[i] >= 'a' && runes[i] <= 'z') {
 			runes[i] = runes[i] - 32
 		}
