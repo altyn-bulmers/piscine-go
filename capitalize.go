e6d49389fccd1b1b2aa53f5593b9cd5e6e60f3e9
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 07:54:12 +0600

    cap

---
+++ capitalize.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Capitalize(s string) string {
+
+	runes := []rune(s)
+	i := 1
+	for i < len(runes)+1 {
+		if (runes[i-1] == ' ' || runes[i-1] == '+' || i == 0) && (runes[i] >= 'a' && runes[i] <= 'z') {
+			runes[i] = runes[i] - 32
+		}
+		i++
+	}
+	return string(runes)
+}
