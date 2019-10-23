735b1a4a0fad759333f317914b8025b208c538d2
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-23 10:18:51 +0600

    isprint

--- isprintable.go
+++ isprintable.go
@@ -1,10 +1,17 @@
 package piscine
 
+func checkprint(a rune) bool {
+	if a >= 0 && a <= 31 {
+		return true
+	}
+	return false
+}
+
 func IsPrintable(str string) bool {
 	runes := []rune(str)
 
 	for i := range runes {
-		if check(runes[i]) == false {
+		if checkprint(runes[i]) == false {
 			return false
 		}
 	}
