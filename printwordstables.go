d5d6e6faaffe15c7cef9f86ee7356f5c080646f7
altyn-bulmers <azhubanchaliyev@gmail.com>
2019-10-24 12:01:26 +0600

    0705

--- printwordstables.go
+++ printwordstables.go
@@ -3,12 +3,6 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(table []string) {
-	length := 0
-
-	for i := range table {
-		length = i + 1
-	}
-
 	for _, v := range table {
 		PRune(v)
 	}
